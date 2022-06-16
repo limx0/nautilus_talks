6eed225f30c2df27467af46b3634ebb4fee564c4
limx0 <bradley.mcelroy@live.com>
2022-06-16 11:23:08 +1000

    Live fixes

--- 20220617/demo/live.py
+++ 20220617/demo/live.py
@@ -1,41 +1,64 @@
-import msgspec.json
-from dotenv import load_dotenv
-from nautilus_trader.live.__main__ import main as live_main, validate
+from nautilus_trader.adapters.interactive_brokers.config import InteractiveBrokersDataClientConfig
+from nautilus_trader.adapters.interactive_brokers.factories import (
+    InteractiveBrokersLiveDataClientFactory,
+)
+from nautilus_trader.config import InstrumentProviderConfig
+from nautilus_trader.config import TradingNodeConfig
+from nautilus_trader.live.node import TradingNode
+from strategy import PairTrader, PairTraderConfig
 
+# Configure the trading node
+config_node = TradingNodeConfig(
+    trader_id="TRADER-001",
+    log_level="DEBUG",
+    data_clients={
+        "IB": InteractiveBrokersDataClientConfig(
+            gateway_host="127.0.0.1",
+            instrument_provider=InstrumentProviderConfig(
+                load_all=True,
+                filters=tuple(
+                    {
+                        "filters": (
+                            tuple({"secType": "STK", "symbol": "SEC0", "exchange": "BVME.ETF"}.items()),
+                            tuple({"secType": "STK", "symbol": "SMH", "exchange": "BVME.ETF"}.items()),
+                        )
+                    }.items()
+                ),
+            )
+        ),
+    },
+    # exec_clients={
+    #     "IB": InteractiveBrokersExecClientConfig(),
+    # },
+    timeout_connection=90.0,
+    timeout_reconciliation=5.0,
+    timeout_portfolio=5.0,
+    timeout_disconnection=5.0,
+    timeout_post_stop=2.0,
+)
 
-def main():
-    raw = msgspec.json.encode(
-        {
-            "trader_id": "TESTER-001",
-            "log_level": "INFO",
-            "data_clients": {
-                "IB": {
-                    "factory_path": "nautilus_trader.adapters.interactive_brokers.factories:InteractiveBrokersLiveDataClientFactory",
-                    "config_path": "nautilus_trader.adapters.interactive_brokers.config:InteractiveBrokersDataClientConfig",
-                    "config": {
-                        "gateway_host": "127.0.0.1",
-                        "instrument_provider": dict(
-                            load_all=True,
-                            filters=tuple({"secType": "CASH", "pair": "EURUSD"}.items()),
-                        ),
-                        "routing": dict(venues={"IDEALPRO"}),
-                    },
-                }
-            },
-            # exec_clients={
-            #     "IB": InteractiveBrokersExecClientConfig(),
-            # },
-            "timeout_connection": 90.0,
-            "timeout_reconciliation": 5.0,
-            "timeout_portfolio": 5.0,
-            "timeout_disconnection": 5.0,
-            "timeout_post_stop": 2.0,
-        }
-    )
-    assert validate(raw)
-    live_main(raw=raw)
+# Instantiate the node with a configuration
+node = TradingNode(config=config_node)
 
+# Configure your strategy
+strategy_config = PairTraderConfig(
+    source_symbol="SEME.IBIS2",
+    target_symbol="SMH.BVME.ETF",
+)
+# Instantiate your strategy
+strategy = PairTrader(config=strategy_config)
 
+# Add your strategies and modules
+node.trader.add_strategy(strategy)
+
+# Register your client factories with the node (can take user defined factories)
+node.add_data_client_factory("IB", InteractiveBrokersLiveDataClientFactory)
+# node.add_exec_client_factory("IB", InteractiveBrokersLiveExecutionClientFactory)
+node.build()
+
+# Stop and dispose of the node with SIGINT/CTRL+C
 if __name__ == "__main__":
-    load_dotenv()
-    main()
+    try:
+        node.start()
+    finally:
+        node.dispose()
